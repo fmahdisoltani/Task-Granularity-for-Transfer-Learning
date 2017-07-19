5fafa375011e976add88e5fe13b86bc90ed7a86f
Ingo F <ingo.fruend@twentybn.com>
2017-07-19 17:21:01 -0400

    fully connected dummy encoder

--- src/main/python/ptcap/encoders.py
+++ src/main/python/ptcap/encoders.py
@@ -6,3 +6,15 @@ class Encoder(nn.Module):
     def forward(self, video_batch):
         """BxCxTxWxH -> BxM"""
         raise NotImplementedError
+
+
+class FullyConnectedEncoder(Encoder):
+
+    def __init__(self, video_dims, repr_dim):
+        super(FullyConnectedEncoder, self).__init__()
+        C, T, W, H = video_dims
+        self.linear = nn.Linear(C*T*W*H, repr_dim)
+
+    def forward(self, video_batch):
+        batch_size = video_batch.size()[0]
+        return self.linear(video_batch.view(batch_size, -1))
