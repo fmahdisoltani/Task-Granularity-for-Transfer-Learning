b3dcadffa3b1eda5fb2b78bed8953bc24edf9b62
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-07-31 12:31:03 -0400

    Refactor SequenceCrossEntropy

--- src/main/python/ptcap/losses.py
+++ src/main/python/ptcap/losses.py
@@ -2,16 +2,14 @@ from torch import nn
 
 
 class SequenceCrossEntropy(nn.Module):
-    loss_function = nn.NLLLoss()
 
-    def __init__(self):
+    def __init__(self, loss=nn.NLLLoss):
         super(SequenceCrossEntropy, self).__init__()
+        self.loss_function = loss()
 
-
-    @classmethod
-    def forward(cls, preds, target):
-        num_step = preds.size()[1]
+    def forward(self, preds, target):
+        batch_size, num_step, _ = preds.size()
         loss = 0.
         for t in range(num_step):
-            loss += cls.loss_function(preds[:, t], target[:, t])
-        return loss / num_step
+            loss += self.loss_function(preds[:, t], target[:, t])
+        return loss / (batch_size*num_step)
