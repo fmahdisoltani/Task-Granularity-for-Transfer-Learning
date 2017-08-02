188e59c3d47733ad9ecc76ff51a08c133c396faf
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-08-02 11:34:37 -0400

    Address one more review comment

--- src/main/python/ptcap/trainers.py
+++ src/main/python/ptcap/trainers.py
@@ -5,18 +5,18 @@ import ptcap.metrics as metrics
 
 class Trainer(object):
     def __init__(self, model,
-                 loss_function, optimizer, num_epoch, num_valid):
+                 loss_function, optimizer, num_epoch, valid_frequency):
         self.model = model
         self.loss_function = loss_function
         self.optimizer = optimizer
         self.num_epoch = num_epoch
-        self.num_valid = num_valid
+        self.valid_frequency = valid_frequency
 
     def train(self, train_dataloader, valid_dataloader):
         for ep in range(self.num_epoch):
             self.run_epoch(train_dataloader, is_training=True)
 
-            if (ep + 1) % self.num_valid == 0:
+            if (ep + 1) % self.valid_frequency == 0:
                 self.run_epoch(valid_dataloader, is_training=False)
 
     def run_epoch(self, dataloader, is_training):
