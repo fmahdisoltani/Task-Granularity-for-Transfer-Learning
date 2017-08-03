ed2c77a07a0c94bc7c5253dd3cfeceec119b7b48
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-08-03 16:45:50 -0400

    Add EncoderDecoder class, a Captioner child

--- src/main/python/ptcap/model/captioners.py
+++ src/main/python/ptcap/model/captioners.py
@@ -1,7 +1,9 @@
+import torch
 from torch import nn
 
 from rtorchn.models.captioning.vid2caption import DeepNet
-
+from ptcap.model.encoders import CNN3dEncoder
+from ptcap.model.decoders import LSTMDecoder
 class Captioner(nn.Module):
 
     def forward(self, video_batch):
@@ -18,7 +20,25 @@ class RtorchnCaptioner(Captioner):
     def __init__(self, vocab_size, go_token=0, batchnorm=True, stateful=False,
                  **kwargs):
         super(RtorchnCaptioner, self).__init__()
-        self.captioner = DeepNet(vocab_size, batchnorm, stateful,**kwargs)
+        self.captioner = DeepNet(vocab_size, batchnorm, stateful, **kwargs)
 
     def forward(self, video_batch):
         return self.captioner.forward(video_batch)
+
+
+class EncoderDecoder(Captioner):
+    def __init__(self, encoder=CNN3dEncoder,
+                 decoder=LSTMDecoder, encoder_output_size=128):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder(encoder_output_size)
+        self.decoder = decoder(embedding_size=97,
+                               hidden_size=encoder_output_size,
+                               vocab_size=34, num_hidden_lstm=71)
+
+    def forward(self, video_batch):
+        videos, captions = video_batch
+        features = self.encoder(videos)
+        h_list = self.decoder(features, captions)
+
+        return h_list
+
