ed6e6a25233f104a4d9b6e9dd31d34201a7d2ea9
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-08-03 16:45:18 -0400

    Add LSTMDecoder

--- src/main/python/ptcap/model/decoders.py
+++ src/main/python/ptcap/model/decoders.py
@@ -1,4 +1,7 @@
+import torch
+
 from torch import nn
+from torch.autograd import Variable
 
 
 class Decoder(nn.Module):
@@ -25,3 +28,47 @@ class FullyConnectedDecoder(Decoder):
             predictions += self.caption_mapping(
                 teacher_captions.view(batch_size, -1))
         return predictions.view(batch_size, -1, self.vocab_size)
+
+
+class LSTMDecoder(nn.Module):
+
+    def __init__(self, embedding_size, hidden_size,
+                 vocab_size, num_hidden_lstm, features=None, use_cuda=False):
+
+        super(LSTMDecoder, self).__init__()
+        self.num_hidden_lstm = num_hidden_lstm
+
+        #Embed each token in vocab to a 128 dimensional vector
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+
+        #batch_first: whether input and output are (batch, seq, feature)
+
+        self.lstm = nn.LSTM(embedding_size, hidden_size, 1, batch_first=True)
+
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.softmax = nn.LogSoftmax()
+        self.use_cuda = use_cuda
+
+    def init_hidden_decoder(self, features):
+        """
+        Hidden states of the LSTM are initialized with features.
+        """
+
+        c0 = features.unsqueeze(0)
+        h0 = features.unsqueeze(0)
+        if self.use_cuda:
+            h0 = h0.cuda(self.gpus[0])
+            c0 = c0.cuda(self.gpus[0])
+        return h0, c0
+
+    def forward(self, features, captions):
+        h0, c0 = self.init_hidden_decoder(features)
+        embedded_captions = self.embedding(captions)
+
+        h_list, _ = self.lstm(embedded_captions, (h0, c0))
+
+        # Project features in a 'vocab_size'-dimensional space
+        h_list1 = torch.stack([self.linear(h) for h in h_list], 0)
+        h_list2 = torch.stack([self.softmax(h) for h in h_list1], 0)
+
+        return h_list2
