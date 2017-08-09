ddc2344c6937afbeefa5dc9e40c48b85bc103637
guillaumebrg <gberer.eclille@gmail.com>
2017-08-09 16:56:48 -0400

    Implement tests

--- src/unittest/python/dataset_tests.py
+++ src/unittest/python/dataset_tests.py
@@ -1,33 +1,69 @@
 import unittest
+from unittest import mock
 
-import torch
-from torch.autograd import Variable
+import numpy as np
+import pandas as pd
+from PIL import Image
 
+from ptcap.data.annotation_parser import JsonParser
 from ptcap.data.dataset import VideoDataset
 from ptcap.data.dataset import JpegVideoDataset
+from ptcap.data.tokenizer import Tokenizer
 
 class TestVideoDataset(unittest.TestCase):
 
-    def setUp(self):
+    FAKE_ANNOT = pd.read_json("""[{"id":1,"file":"11/vid1.mp4",
+            "duration":"5.4","width":720,"height":480,
+            "label":"ONE HAND AND THE OTHER ", 
+            "template":"ONE HAND AND THE OTHER",
+            "placeholders":["the table","coffee cup"],
+            "external_worker_id":"A2YD53VKHR3BED"},
+            {"id":2,"file":"21/vid2.mp4",
+            "duration":"5.4","width":720,"height":480,
+            "label":"Tilting the table with coffee cup on it",
+            "template":"Tilting [something] with [something] on it",
+            "placeholders":["the table","coffee cup"],
+            "external_worker_id":"A2YD53VKHR3BED"},
+            {"id":1,"file":"31/vid3.mp4",
+            "duration":"5.4","width":720,"height":480,
+            "label":"THREE HANDS ARE THERE",
+            "template":"THREE HANDS ARE THERE",
+            "placeholders":["the table","coffee cup"],
+            "external_worker_id":"A2YD53VKHR3BED"}
+            ]""")
+
+    @mock.patch('ptcap.data.annotation_parser.JsonParser.open_annotation',
+                return_value=FAKE_ANNOT)
+    def setUp(self, mock_open_annot):
         # Create a fake video along with a string caption
-        pass
+        captions = [p for p in self.FAKE_ANNOT["label"]]
+        self.tokenizer = Tokenizer(captions)
+        self.annotation_parser = JsonParser('', '')
+        self.dataset = VideoDataset(self.annotation_parser, self.tokenizer)
 
-    # tests:
-    # __getitem__ returns video, string, tokenized_string tuple
-    # _getvideo returns the videos with the correct size
-    # __len__ returns the correct number of videos
-
-    def test_getitem(self):
-        pass
+    @mock.patch('ptcap.data.dataset.VideoDataset._get_video',
+                return_value= np.random.rand(5, 7, 11, 3))
+    @mock.patch('ptcap.data.dataset.VideoDataset._get_tokenized_caption',
+                return_value=np.random.rand(5, 7))
+    def test_getitem(self, mock_get_video, mock_get_tokenized_caption):
+        for i, target_caption in enumerate(self.annotation_parser.get_captions()):
+            with self.subTest('check sample %d' % i):
+                video, caption, tokenized_caption = self.dataset[i]
+                self.assertEqual(caption, target_caption)
+                self.assertEqual(video.shape, (5, 7, 11, 3))
+                self.assertEqual(tokenized_caption.shape, (5, 7))
 
     def test_len(self):
-        pass
+        self.assertEqual(len(self.dataset), 3)
 
 
-class TestJpegVideoDataset(unittest.TestCase):
+class TestJpegVideoDataset(TestVideoDataset):
+    PIL_IMAGE = Image.new("RGB", (256, 256), "white")
 
-    def setUp(self):
-        pass
-
-    def test_getvideo(self):
-        pass
+    @mock.patch('PIL.Image.open', return_value=PIL_IMAGE)
+    @mock.patch('glob.glob', return_value=[0 for i in range(13)])
+    def test_getvideo(self, mock_pil, mock_glob):
+        dataset = JpegVideoDataset([128,128], Image.BICUBIC,
+                                   self.annotation_parser, self.tokenizer)
+        video = dataset._get_video(0)
+        self.assertEqual(video.shape, (13, 128, 128, 3))
