700267da77eb053cb151e6659449f5ec137c28b4
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-08-11 16:47:50 -0400

    Refactor token_level_accuracy function

--- src/main/python/ptcap/metrics.py
+++ src/main/python/ptcap/metrics.py
@@ -1,6 +1,20 @@
 
-def token_level_accuracy(captions, predictions):
-    batch_size, num_steps = captions.size()
-    equal_values = captions.eq(predictions).sum().float()
-    accuracy = equal_values * 100.0 / (batch_size * num_steps)
+# def token_level_accuracy(captions, predictions):
+#     batch_size, num_steps = captions.size()
+#     equal_values = captions.eq(predictions).sum().float()
+#     accuracy = equal_values * 100.0 / (batch_size * num_steps)
+#     return accuracy
+#
+#
+# def first_token_accuracy(captions, predictions):
+#     batch_size, _ = captions.size()
+#     equal_values = captions[:, 0].eq(predictions[:, 0]).sum().float()
+#     accuracy = equal_values * 100.0 / batch_size
+#     return accuracy
+
+
+def token_level_accuracy(captions, predictions, num_tokens=-1):
+    batch_size, _ = captions.size()
+    equal_values = captions[:, 0:num_tokens].eq(predictions[:, 0:num_tokens])
+    accuracy = equal_values.sum().float() * 100.0 / batch_size
     return accuracy
