912757068796a80e07622f534b9839499b97c177
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-08-12 16:17:46 -0400

    Fix division in token_level accuracy

--- src/main/python/ptcap/metrics.py
+++ src/main/python/ptcap/metrics.py
@@ -1,6 +1,8 @@
 
 def token_level_accuracy(captions, predictions, num_tokens=None):
-    batch_size, _ = captions.size()
+    batch_size, num_step = captions.size()
+    if not num_tokens:
+        num_tokens = num_step
     equal_values = captions[:, 0:num_tokens].eq(predictions[:, 0:num_tokens])
-    accuracy = equal_values.sum().float() * 100.0 / batch_size
+    accuracy = equal_values.sum().float() * 100.0 / (batch_size * num_tokens)
     return accuracy
