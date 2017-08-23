5c6d758ad008fa12a09681b3951615ab7a26266e
wgharbieh <wgharbieh@hotmail.com>
2017-08-23 10:41:23 -0400

    Add a test and fix load_dictionaries

--- src/unittest/python/tokenizer_tests.py
+++ src/unittest/python/tokenizer_tests.py
@@ -9,6 +9,7 @@ from ptcap.data.tokenizer import Tokenizer
 
 class TestTokenizer(unittest.TestCase):
     def setUp(self):
+        self.phrase = "THERE IS ONE HAND"
         self.arguments = """[{"id":1,"file":"11/vid1.mp4",
         "duration":"5.4","width":720,"height":480,
         "label":"ONE HAND AND THE OTHER ", 
@@ -46,16 +47,14 @@ class TestTokenizer(unittest.TestCase):
                          len(self.captions_vocab))
 
     def test_encode_decode(self):
-        phrase = "THERE IS ONE HAND"
-        phrase_encoded = self.tokenizer.encode_caption(phrase)
+        phrase_encoded = self.tokenizer.encode_caption(self.phrase)
         phrase_decoded = self.tokenizer.decode_caption(phrase_encoded)
         self.assertEqual(phrase_decoded[:5], ['THERE',
                                               Tokenizer.UNK, 'ONE',
                                              'HAND', Tokenizer.END])
 
     def test_max_len(self):
-        phrase = "THERE IS ONE HAND"
-        phrase_encoded = self.tokenizer.encode_caption(phrase)
+        phrase_encoded = self.tokenizer.encode_caption(self.phrase)
         self.assertEqual(len(phrase_encoded), self.tokenizer.maxlen)
 
     @tempdir()
@@ -67,3 +66,14 @@ class TestTokenizer(unittest.TestCase):
                          loading_tokenizer.caption_dict)
         self.assertEqual(self.tokenizer.inv_caption_dict,
                          loading_tokenizer.inv_caption_dict)
+
+    @tempdir()
+    def test_tokenize_from_loaded_tokenizer(self, temp_dir):
+        self.tokenizer.save_dictionaries(temp_dir.path)
+        loading_tokenizer = Tokenizer()
+        loading_tokenizer.load_dictionaries(temp_dir.path)
+        phrase_encoded = loading_tokenizer.encode_caption(self.phrase)
+        phrase_decoded = self.tokenizer.decode_caption(phrase_encoded)
+        self.assertEqual(phrase_decoded[:5], ['THERE',
+                                              Tokenizer.UNK, 'ONE',
+                                             'HAND', Tokenizer.END])
