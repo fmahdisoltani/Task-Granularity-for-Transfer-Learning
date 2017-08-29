453a53e73353e54b6af9bbcf49399fde666c4777
wgharbieh <wgharbieh@hotmail.com>
2017-08-29 17:10:19 -0400

    Make tests pass and fix imports

--- src/main/python/ptcap/trainers.py
+++ src/main/python/ptcap/trainers.py
@@ -8,10 +8,8 @@ from collections import OrderedDict
 from torch.autograd import Variable
 
 from ptcap.checkpointers import Checkpointer
-from ptcap.scores import ScoresOperator
-from ptcap.scores import token_accuracy
-from ptcap.scores import loss_to_numpy
-from ptcap.scores import first_token_accuracy
+from ptcap.metrics import (first_token_accuracy, loss_to_numpy, ScoresOperator,
+                           token_accuracy)
 
 
 class Trainer(object):
--- src/unittest/python/metric_tests.py
+++ src/unittest/python/metric_tests.py
@@ -7,11 +7,10 @@ from collections import OrderedDict
 
 from torch.autograd import Variable
 
-from ptcap.metrics import MetricsOperator
-from ptcap.metrics import token_level_accuracy
+from ptcap.metrics import (ScoresOperator, token_level_accuracy)
 
 
-class MetricTests(unittest.TestCase):
+class ScoreTests(unittest.TestCase):
 
     def setUp(self):
         self.num_epochs = 3
@@ -20,56 +19,56 @@ class MetricTests(unittest.TestCase):
         """
             Test when the moving average is getting random input.
         """
-        metrics_operator = MetricsOperator(
+        scores_operator = ScoresOperator(
             OrderedDict([("loss", lambda x: x), ("Add1", lambda x: x + 1)]))
         input_list = []
 
         for count in range(self.num_epochs):
             random_num = np.random.rand()
             input_list.append(random_num)
-            metrics_dict = metrics_operator.run_metrics(random_num)
-            metrics_dict = metrics_operator.moving_average(metrics_dict,
+            scores_dict = scores_operator.run_scores(random_num)
+            scores_dict = scores_operator.moving_average(scores_dict,
                                                            count + 1)
             expected = np.mean(input_list)
 
-            self.assertAlmostEqual(metrics_dict["average_loss"], expected, 14)
+            self.assertAlmostEqual(scores_dict["average_loss"], expected, 14)
 
     def test_input_same(self):
         """
             Test when the moving average is getting the same input (0 and 1
             here).
         """
-        metrics_operator = MetricsOperator(
+        scores_operator = ScoresOperator(
             OrderedDict([("loss", lambda x: x), ("Add1", lambda x: x + 1)]))
 
         for value in range(2):
             for count in range(self.num_epochs):
-                metrics_dict = metrics_operator.run_metrics(value)
-                metrics_dict = metrics_operator.moving_average(metrics_dict,
+                scores_dict = scores_operator.run_scores(value)
+                scores_dict = scores_operator.moving_average(scores_dict,
                                                                count + 1)
-                self.assertEqual(metrics_dict["average_loss"], value)
+                self.assertEqual(scores_dict["average_loss"], value)
 
-    def test_run_metrics(self):
-        metrics_operator = MetricsOperator(OrderedDict([("add1",
+    def test_run_scores(self):
+        scores_operator = ScoresOperator(OrderedDict([("add1",
                                                          lambda x: x + 1)]))
-        metrics_dict = metrics_operator.run_metrics(1)
-        self.assertEqual(metrics_dict["add1"], 2)
+        scores_dict = scores_operator.run_scores(1)
+        self.assertEqual(scores_dict["add1"], 2)
 
-    def test_compute_metrics(self):
-        metrics_operator = MetricsOperator(OrderedDict([("add1",
-                                                         lambda x: x + 1)]))
-        for count in range(self.num_epochs):
-            metrics_dict = metrics_operator.compute_metrics(1, count + 1)
-            self.assertEqual(metrics_dict["add1"], 2)
-            self.assertEqual(metrics_dict["average_add1"], 2)
-
-    def test_get_average_metrics(self):
-        metrics_operator = MetricsOperator(OrderedDict([("add1",
+    def test_compute_scores(self):
+        scores_operator = ScoresOperator(OrderedDict([("add1",
                                                          lambda x: x + 1)]))
         for count in range(self.num_epochs):
-            metrics_operator.compute_metrics(1, count + 1)
-        average_metrics = metrics_operator.get_average_metrics()
-        self.assertEqual(list(average_metrics.keys()), ["average_add1"])
+            scores_dict = scores_operator.compute_scores(1, count + 1)
+            self.assertEqual(scores_dict["add1"], 2)
+            self.assertEqual(scores_dict["average_add1"], 2)
+
+    def test_get_average_scores(self):
+        scores_operator = ScoresOperator(OrderedDict([("add1",
+                                                         lambda x: x + 1)]))
+        for count in range(self.num_epochs):
+            scores_operator.compute_scores(1, count + 1)
+        average_scores = scores_operator.get_average_scores()
+        self.assertEqual(list(average_scores.keys()), ["average_add1"])
 
 
 class TestTokenLevelAccuracy(unittest.TestCase):
