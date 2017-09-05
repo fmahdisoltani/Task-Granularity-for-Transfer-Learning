757d0cb67ba1d413cb1f01f127467af87d200911
wgharbieh <wgharbieh@hotmail.com>
2017-09-05 10:20:05 -0400

    Add checkpointer_tests.py skeleton

---
+++ src/unittest/python/checkpointer_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+class CheckpointerTests(unittest.TestCase):
+
+    def test_load_model(self):
+        pass
+
+    def test_save_best(self):
+        pass
+
+    def test_save_latest(self):
+        pass
+
+    def test_save_meta(self):
+        pass
+
+    def test_save_value_csv(self):
+        pass
