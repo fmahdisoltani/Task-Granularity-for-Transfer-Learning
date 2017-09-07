f641a04c9041f37ed27ffd726aeeca0a6095550f
wgharbieh <wgharbieh@hotmail.com>
2017-09-06 20:14:48 -0400

    Reorder imports in build.py

--- build.py
+++ build.py
@@ -1,26 +1,27 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import init, use_plugin
 
+use_plugin("pypi:pybuilder_research_plugin")
 use_plugin("python.core")
-use_plugin("python.unittest")
-use_plugin("python.integrationtest")
-use_plugin("python.install_dependencies")
-use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("python.flake8")
+use_plugin("python.install_dependencies")
+use_plugin("python.integrationtest")
 use_plugin("python.pycharm")
-use_plugin("pypi:pybuilder_research_plugin")
+use_plugin("python.unittest")
 
-name = "ptcap"
 default_task = "publish"
+name = "ptcap"
 
 
 @init
 def set_properties(project):
-    project.depends_on('pyfackel')
     project.depends_on('pandas')
-    project.depends_on('testfixtures')
+    project.depends_on('pyfackel')
     project.depends_on('rtorchn', url=('git+ssh://git@github.com/TwentyBN/'
                                        '20bn-rtorchn.git'))
-    project.set_property('flake8_verbose_output', "True")
+    project.depends_on('testfixtures')
+
     project.set_property('coverage_threshold_warn', 0)
+    project.set_property('flake8_verbose_output', "True")
     project.set_property('integrationtest_inherit_environment', True)
