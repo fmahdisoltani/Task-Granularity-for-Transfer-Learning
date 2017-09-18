Clip gradients and fix minor tensorboard issue

src/main/python/ptcap/model/decoders.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 
-from ptcap.tensorboardY import (update_dict, register_grad)
+from ptcap.tensorboardY import update_dict
 
 
 class Decoder(nn.Module):
@@ -53,7 +53,6 @@ class LSTMDecoder(Decoder):
         self.gpus = gpus
         self.go_token = go_token
 
-        self.gradients = {}
         self.hidden = {}
 
     def init_hidden(self, features):
@@ -103,7 +102,6 @@ class LSTMDecoder(Decoder):
         var_list = [lstm_outputs, lstm_out_projections, probs]
 
         update_dict(self.hidden, zip(key_list, var_list), num_step)
-        register_grad(self.gradients, zip(key_list, var_list), num_step)
 
         return probs
 

src/main/python/ptcap/model/encoders.py
@@ -1,12 +1,10 @@
-import numpy as np
-
 import torch
 import torch.nn as nn
 
 from torch.autograd import Variable
 
 from ptcap.model.layers import CNN3dLayer
-from ptcap.tensorboardY import register_grad, update_dict
+from ptcap.tensorboardY import update_dict
 
 
 class Encoder(nn.Module):
@@ -52,7 +50,6 @@ class CNN3dEncoder(Encoder):
 
         self.pool4 = nn.MaxPool3d((1, 6, 6))
 
-        self.gradients = {}
         self.hidden = {}
 
     def forward(self, videos):
@@ -76,8 +73,6 @@ class CNN3dEncoder(Encoder):
         self.hidden["mean_pool"] = self.hidden["pool4_layer"].mean(2)
         self.hidden["features"] = self.hidden["mean_pool"].view(
                                 self.hidden["mean_pool"].size()[0:2])
-
-        register_grad(self.gradients, self.hidden.items())
 
         return self.hidden["features"]
 
@@ -119,7 +114,6 @@ class CNN3dLSTMEncoder(Encoder):
         self.lstm = nn.LSTM(input_size=128, hidden_size=self.num_features,
                             num_layers=self.num_layers, batch_first=True)
 
-        self.gradients = {}
         self.hidden = {}
 
     def init_hidden(self, batch_size):
@@ -159,8 +153,6 @@ class CNN3dLSTMEncoder(Encoder):
 
         vars_tuple = [("encoder_lstm_outputs", lstm_outputs)]
 
-        register_grad(self.gradients, self.hidden.items())
         update_dict(self.hidden, vars_tuple, h.size(1))
-        register_grad(self.gradients, vars_tuple, h.size(1))
 
         return self.hidden["features"]

src/main/python/ptcap/trainers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 
 import ptcap.loggers as lg
@@ -116,11 +118,10 @@ class Trainer(object):
 
                 self.model.zero_grad()
                 loss.backward()
+                torch.nn.utils.clip_grad_norm(self.model.parameters(), 1)
                 self.optimizer.step()
 
-                model_gradients = [self.model.encoder.gradients,
-                                   self.model.decoder.gradients]
-                self.writer.add_variables(model_gradients, global_step)
+                self.writer.add_gradients(self.model, global_step)
 
             # convert probabilities to predictions
             _, predictions = torch.max(probs, dim=2)