208ab4bf1ebd467b5ca98b8618208cfd906da120
wgharbieh <waseem.gharbieh@twentybn.com>
2017-10-06 16:18:50 -0400

    Create config file inside config_parser_tests.py

--- src/unittest/python/config_parser_tests.py
+++ src/unittest/python/config_parser_tests.py
@@ -1,7 +1,8 @@
 import os
+import shutil
 import unittest
 
-from testfixtures import tempdir
+from testfixtures import TempDirectory
 
 from ptcap.data.config_parser import YamlConfig
 
@@ -9,21 +10,27 @@ from ptcap.data.config_parser import YamlConfig
 class ConfigParserTest(unittest.TestCase):
 
     def setUp(self):
-        config_path = "src/main/configs/config_parser.yaml"
-        path = os.path.join(os.getcwd(), config_path)
-        self.config_parser = YamlConfig(path)
+        self.config_path = os.path.join(TempDirectory().path, "temp_config")
+        config_contents = ("path: some_path\n\nnested:\n  type: some_type\n  "
+                           "kwargs: {kwarg_1: 1, kwarg_2: kwarg2}")
+        with open(self.config_path, "w") as f:
+            f.write(config_contents)
 
-    def test_config_dict(self):
-        config_dict = self.config_parser.config_dict
+    def test_parse_config_file(self):
+        config_obj = YamlConfig(self.config_path)
+        config_dict = config_obj.config_dict
         self.assertEqual(config_dict["path"], "some_path")
         self.assertEqual(config_dict["nested"]["type"], "some_type")
         self.assertEqual(config_dict["nested"]["kwargs"]["kwarg_1"], 1)
         self.assertEqual(config_dict["nested"]["kwargs"]["kwarg_2"], "kwarg2")
 
     def test_get_values_from_config_parser(self):
-        self.assertEqual(self.config_parser.get("path"), "some_path")
-        self.assertEqual(self.config_parser.get("nested", "type"), "some_type")
+        config_obj = YamlConfig(self.config_path)
+        self.assertEqual(config_obj.get("path"), "some_path")
+        self.assertEqual(config_obj.get("nested", "type"), "some_type")
+        self.assertEqual(config_obj.get("nested", "kwargs", "kwarg_1"), 1)
         self.assertEqual(
-            self.config_parser.get("nested", "kwargs", "kwarg_1"), 1)
-        self.assertEqual(
-            self.config_parser.get("nested", "kwargs", "kwarg_2"), "kwarg2")
+            config_obj.get("nested", "kwargs", "kwarg_2"), "kwarg2")
+
+    def tearDown(self):
+        shutil.rmtree(self.config_path)
