389ec39ea3cd39be948fd6e14de69904baf08d99
wgharbieh <waseem.gharbieh@twentybn.com>
2017-10-06 16:55:56 -0400

    Add more tests

--- src/unittest/python/config_parser_tests.py
+++ src/unittest/python/config_parser_tests.py
@@ -7,30 +7,39 @@ from testfixtures import TempDirectory
 from ptcap.data.config_parser import YamlConfig
 
 
+def check_obj_dict_and_dict_equal(config_obj, config_dict):
+    pass
+
+
 class ConfigParserTest(unittest.TestCase):
 
     def setUp(self):
         self.config_path = os.path.join(TempDirectory().path, "temp_config")
         config_contents = ("path: some_path\n\nnested:\n  type: some_type\n  "
                            "kwargs: {kwarg_1: 1, kwarg_2: kwarg2}")
+        self.config_dict = {
+            "path": "some_path",
+            "nested": {"type": "some_type",
+                       "kwargs": {"kwarg_1": 1, "kwarg_2": "kwarg2"}}}
         with open(self.config_path, "w") as f:
             f.write(config_contents)
 
     def test_parse_config_file(self):
         config_obj = YamlConfig(self.config_path)
         config_dict = config_obj.config_dict
-        self.assertEqual(config_dict["path"], "some_path")
-        self.assertEqual(config_dict["nested"]["type"], "some_type")
-        self.assertEqual(config_dict["nested"]["kwargs"]["kwarg_1"], 1)
-        self.assertEqual(config_dict["nested"]["kwargs"]["kwarg_2"], "kwarg2")
+        self.assertEqual(config_dict, self.config_dict)
 
     def test_get_values_from_config_parser(self):
         config_obj = YamlConfig(self.config_path)
-        self.assertEqual(config_obj.get("path"), "some_path")
-        self.assertEqual(config_obj.get("nested", "type"), "some_type")
-        self.assertEqual(config_obj.get("nested", "kwargs", "kwarg_1"), 1)
-        self.assertEqual(
-            config_obj.get("nested", "kwargs", "kwarg_2"), "kwarg2")
+        check_obj_dict_and_dict_equal(config_obj, self.config_dict)
+
+    def test_save_config_file(self):
+        temp_dir = TempDirectory().path
+        temp_file = "temp_config2"
+        config_obj = YamlConfig(self.config_path)
+        config_obj.save(temp_dir, temp_file)
+        config_obj.parse(os.path.join(temp_dir, temp_file))
+        self.assertEqual(config_obj.config_dict, self.config_dict)
 
     def tearDown(self):
         shutil.rmtree(self.config_path)
