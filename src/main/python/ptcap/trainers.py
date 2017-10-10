5bc1ef21de7c271fc359c8d9c64f4a31d114c01a
wgharbieh <waseem.gharbieh@twentybn.com>
2017-10-10 12:39:20 -0400

    Take clip_grad values from config file

--- src/main/python/ptcap/trainers.py
+++ src/main/python/ptcap/trainers.py
@@ -13,7 +13,7 @@ from ptcap.scores import (ScoresOperator, caption_accuracy,
 class Trainer(object):
     def __init__(self, model, loss_function, scheduler, tokenizer, logger,
                  writer, checkpoint_path, folder=None, filename=None,
-                 gpus=None):
+                 gpus=None, clip_grad=None):
 
         self.use_cuda = True if gpus else False
         self.gpus = gpus
@@ -27,6 +27,7 @@ class Trainer(object):
         self.loss_function = (loss_function.cuda(gpus[0])
                               if self.use_cuda else loss_function)
 
+        self.clip_grad = clip_grad
         self.logger = logger
         self.tokenizer = tokenizer
         self.score = self.scheduler.best
@@ -141,7 +142,9 @@ class Trainer(object):
 
                 self.model.zero_grad()
                 loss.backward()
-                torch.nn.utils.clip_grad_norm(self.model.parameters(), 1)
+                if self.clip_grad is not None:
+                    torch.nn.utils.clip_grad_norm(self.model.parameters(),
+                                                  self.clip_grad)
 
                 if (self.tensorboard_frequency is not None and
                         global_step % self.tensorboard_frequency == 0):
