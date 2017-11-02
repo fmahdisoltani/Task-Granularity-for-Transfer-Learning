71f9c4593192593dfadab049b685f1d12217ea1d
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2017-11-02 16:31:34 -0400

    Create main.py

---
+++ src/main/python/scripts/main.py
@@ -0,0 +1,24 @@
+"""Training script.
+Usage:
+  train.py <config_path>
+  train.py (-h | --help)
+Options:
+  <configpath>           Path to a config file.
+  -h --help              Show this screen.
+"""
+
+from docopt import docopt
+
+from ptcap.data.config_parser import YamlConfig
+from ptcap.scripts.train import train_model
+
+
+if __name__ == "__main__":
+    # Get argument
+    args = docopt(__doc__)
+
+    # Build a dictionary that contains fields of config file
+    config_obj = YamlConfig(args["<config_path>"])
+
+    # Run captioning model
+    train_model(config_obj)
