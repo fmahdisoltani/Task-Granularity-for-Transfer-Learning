c8ee16b8587db00b2e94b436c438269221d2ab2e
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2017-11-13 17:19:32 -0500

    Not to mean pool in pretrained_encoders

--- src/main/python/ptcap/model/pretrained_encoders.py
+++ src/main/python/ptcap/model/pretrained_encoders.py
@@ -1,9 +1,12 @@
 import torch
+import torch.nn as nn
 from .encoders import Encoder
 from rtorchn.core.networks import (FullyConvolutionalNet, JesterNet)
-from rtorchn.core.networks import  BiJesterNetII
+from rtorchn.core.networks import BiJesterNetII
 from .encoders import Encoder
 from rtorchn.core.networks.resnets import InflatedResNet18
+
+import torch.nn.functional as F
 
 
 class PretrainedEncoder(Encoder):
@@ -29,9 +32,7 @@ class PretrainedEncoder(Encoder):
 
 
 class FCEncoder(PretrainedEncoder):
-
     def __init__(self, pretrained_path=None, freeze=False):
-
         # Hardcoded encoder for using FullyConvolutionalNet from 20bn_rtorchn
 
         # it thinks it's getting num_features, but it's not. what is happening
@@ -52,14 +53,12 @@ class FCEncoder(PretrainedEncoder):
 
 
 class JesterEncoder(PretrainedEncoder):
-
     """
         Hardcoded encoder for using JesterNet from 20bn_rtorchn
         num_classes = 329 means this class expects the "supermodel" version.
     """
 
     def __init__(self, pretrained_path=None, freeze=False):
-
         encoder_output_size = 256
         num_classes = 329
         encoder_args = (num_classes, encoder_output_size)
@@ -71,39 +70,42 @@ class JesterEncoder(PretrainedEncoder):
     def forward(self, video_batch):
         features = self.encoder.extract_features(video_batch)
         return features.mean(dim=1)
-    
-    
+
+
 class BIJesterEncoder(PretrainedEncoder):
-#
-#      """
-# #         Hardcoded encoder for using BIJesterNet from 20bn_rtorchn
- #         num_classes = 174 means this class expects the "supermodel" version.
- #         Encoder output size in this version is 1024
- #     """
 
-      def __init__(self, pretrained_path=None, freeze=False):
+    """
+            Hardcoded encoder for using BIJesterNet from 20bn_rtorchn
+            num_classes = 174 means this class expects the "supermodel" version.
+            Encoder output size in this version is 1024
+    """
 
-          encoder_output_size = 1024
-          num_classes = 178
-          encoder_args = (num_classes, encoder_output_size)
-          super(BIJesterEncoder, self).__init__(encoder=BiJesterNetII,
-                                                encoder_args=encoder_args,
-                                                pretrained_path=pretrained_path,
-                                                freeze=freeze)
+    def __init__(self, pretrained_path=None, freeze=False):
+        encoder_output_size = 1024
+        relu_output_size = 1024
+        num_classes = 178
+        encoder_args = (num_classes, encoder_output_size)
 
-      def forward(self, video_batch):
-          features = self.encoder.extract_features(video_batch)
-          return features.mean(dim=1)
+        # FC layer on top of features
+
+        super(BIJesterEncoder, self).__init__(encoder=BiJesterNetII,
+                                              encoder_args=encoder_args,
+                                              pretrained_path=pretrained_path,
+                                              freeze=freeze)
+
+    def forward(self, video_batch):
+        features = self.encoder.extract_features(video_batch)
+        return features
+
 
 class Resnet18Encoder(PretrainedEncoder):
-
     def __init__(self, pretrained_path=None, freeze=False, ):
         num_classes = 1363
         encoder_args = (num_classes,)
         super(Resnet18Encoder, self).__init__(encoder=InflatedResNet18,
-                                               encoder_args=encoder_args,
-                                               pretrained_path=pretrained_path,
-                                               freeze=freeze)
+                                              encoder_args=encoder_args,
+                                              pretrained_path=pretrained_path,
+                                              freeze=freeze)
 
     def forward(self, video_batch):
         features = self.encoder.extract_features(video_batch)
