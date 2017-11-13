9fcc717e0bd1811ec8b941e24eab24186e97b3d4
wgharbieh <wgharbieh@hotmail.com>
2017-11-13 08:25:11 -0500

    Address reviews

--- src/main/python/ptcap/losses.py
+++ src/main/python/ptcap/losses.py
@@ -8,7 +8,7 @@ class SequenceCrossEntropy(nn.Module):
         self.loss_function = loss()
 
     def forward(self, preds, target):
-        batch_size, num_step, _ = preds.size()
+        num_step = preds.size(1)
         loss = 0.
         for t in range(num_step):
             loss += self.loss_function(preds[:, t], target[:, t])
