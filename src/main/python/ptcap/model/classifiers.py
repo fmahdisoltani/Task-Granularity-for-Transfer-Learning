02e0461dbe320662af5d86087b843f811b94776e
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2017-11-12 23:23:22 -0500

    Classifiers

--- src/main/python/ptcap/model/classifiers.py
+++ src/main/python/ptcap/model/classifiers.py
@@ -1,72 +1,33 @@
 import torch
 from torch import nn
-
-from rtorchn.core.networks import RtorchnCaptioner as RtorchnCap
-from ptcap.model.encoders import (CNN3dEncoder, CNN3dLSTMEncoder)
-from ptcap.model.decoders import DecoderBase
+from ptcap.model.encoders import (CNN3dEncoder,
+                                  CNN3dLSTMEncoder)
 
 
-class Classifier(nn.Module):
-
-    def forward(self, video_batch, use_teacher_forcing):
-        """BxCxTxWxH -> BxTxV"""
-        raise NotImplementedError
-
-
-class RtorchnCaptioner(Classifier):
-    """
-    This class is a wrapper for rtorchn captioning model.
-    Typical kwargs: is_training=True, use_cuda=False, gpus=[0]
-    """
-
-    def __init__(self, vocab_size):
-        super(RtorchnCaptioner, self).__init__()
-        self.captioner = RtorchnCap(vocab_size)
-
-    def forward(self, video_batch, use_teacher_forcing=True):
-        return self.captioner.forward(video_batch)
-
-
-class EncoderDecoder(Classifier):
-    def __init__(self, encoder, decoder, encoder_args=None, encoder_kwargs=None,
-                 decoder_args=None, decoder_kwargs=None, gpus=None):
-
-        print("gpus: {}".format(gpus))
-        super().__init__()
+class C3dClassifier(nn.Module):
+    def __init__(self, encoder=CNN3dEncoder, encoder_args=(), num_classes=7, gpus=None):
+        super(C3dClassifier, self).__init__()
         self.use_cuda = True if gpus else False
         self.gpus = gpus
-        encoder_args = encoder_args or ()
-        encoder_kwargs = encoder_kwargs or {}
-        decoder_args = decoder_args or ()
-        decoder_kwargs = decoder_kwargs or {}
-        self.encoder = encoder(*encoder_args, **encoder_kwargs)
-        self.decoder = decoder(*decoder_args, **decoder_kwargs)
 
-        self.activations = {}
-        self.register_forward_hook(self.merge_activations)
+        #TODO: 128 is encoder num_feature, fix it to receive it
+        self.linear = nn.Linear(128, num_classes)
+        self.encoder = encoder(*encoder_args)
+        self.logsoftmax = nn.LogSoftmax()
 
-    def forward(self, video_batch, use_teacher_forcing):
-        videos, labels = video_batch
+
+    def forward(self, video_batch, use_teacher_forcing=False):
+        videos, captions = video_batch
         features = self.encoder(videos)
-        probs = self.decoder(features, captions, use_teacher_forcing)
-
+        probs = self.predict(features)
         return probs
 
-    def merge_activations(self, module, input_tensor, output_tensor):
-        self.activations = dict(self.encoder.activations,
-                                **self.decoder.activations)
 
+    def predict(self, features):
 
-class CNN3dLSTM(EncoderDecoder):
-    def __init__(self, encoder_output_size=128, embedding_size=31,
-                 vocab_size=33, num_hidden_lstm=71, go_token=0, gpus=None):
+        # The output should be num_classes dimensional now
+        fc_output = self.linear(features)
+        probs = self.logsoftmax(fc_output)
 
-        decoder_args = (embedding_size, encoder_output_size,
-                        vocab_size, num_hidden_lstm, go_token, gpus)
-
-        encoder_args = (encoder_output_size, gpus)
-
-        super(CNN3dLSTM, self).__init__(CNN3dLSTMEncoder, DecoderBase,
-                                        encoder_args=encoder_args,
-                                        decoder_args=decoder_args,
-                                        gpus=gpus)
+        _, preds = torch.max(probs, dim=1)
+        return probs
