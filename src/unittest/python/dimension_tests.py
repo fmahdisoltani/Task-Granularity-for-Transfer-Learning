fc34fff34f61da8d05dc51589b77d87ac63c62d5
wgharbieh <wgharbieh@hotmail.com>
2017-11-14 15:55:38 -0500

    Refactor num_features to encoder_output_size

--- src/unittest/python/dimension_tests.py
+++ src/unittest/python/dimension_tests.py
@@ -16,28 +16,30 @@ class TestDimensions(unittest.TestCase):
         self.batch_size = 2
         self.vocab_size = 5
         self.caption_len = 4
-        self.num_features = 7
+        self.encoder_output_size = 7
         self.arguments = {
-            "FullyConnectedEncoder": (((3, 10, 96, 96), self.num_features), {}),
-            "CNN3dEncoder": ((self.num_features,), {}),
-            "CNN3dLSTMEncoder": ((self.num_features,), {}),
-            "PretrainedEncoder": ((encoders.CNN3dLSTMEncoder, (self.num_features,),
-                                   ), {}),
+            "FullyConnectedEncoder": (((3, 10, 96, 96),
+                                       self.encoder_output_size), {}),
+            "CNN3dEncoder": ((self.encoder_output_size,), {}),
+            "CNN3dLSTMEncoder": ((self.encoder_output_size,), {}),
+            "PretrainedEncoder": ((encoders.CNN3dLSTMEncoder,
+                                   (self.encoder_output_size,),), {}),
 
             "FullyConnectedMapper": ((4, 10), {}),
-            "DecoderBase": ((17, self.num_features, 1, self.vocab_size,
+            "DecoderBase": ((17, self.encoder_output_size, 1, self.vocab_size,
                              self.caption_len), {}),
-            "LSTMDecoder": ((17, self.num_features, 1, self.vocab_size,
+            "LSTMDecoder": ((17, self.encoder_output_size, 1, self.vocab_size,
                              self.caption_len), {}),
-            "CoupledLSTMDecoder": ((17, self.num_features, 1, self.vocab_size,
-                             self.caption_len), {}),
+            "CoupledLSTMDecoder": ((17, self.encoder_output_size, 1,
+                                    self.vocab_size, self.caption_len), {}),
 
             "RtorchnCaptioner": ((self.vocab_size,), {}),
             "EncoderDecoder": (
                 (encoders.CNN3dLSTMEncoder, decoders.LSTMDecoder),
-                {"encoder_kwargs": {"encoder_output_size":  self.num_features},
+                {"encoder_kwargs": {"encoder_output_size":
+                                        self.encoder_output_size},
                  "decoder_kwargs": {"embedding_size": 17,
-                                    "hidden_size": self.num_features,
+                                    "hidden_size": self.encoder_output_size,
                                     "vocab_size": self.vocab_size,
                                     "num_lstm_layers": 1,
                                     "num_step": self.caption_len}}),
@@ -57,7 +59,7 @@ class TestDimensions(unittest.TestCase):
                 encoded = encoder(video_batch)
 
                 self.assertEqual(encoded.size()[0], self.batch_size)
-                self.assertEqual(encoded.size()[1], self.num_features)
+                self.assertEqual(encoded.size()[1], self.encoder_output_size)
                 self.assertEqual(len(encoded.size()), 2)
 
     def test_mappers(self):
@@ -78,7 +80,7 @@ class TestDimensions(unittest.TestCase):
     def test_decoders(self):
         decoder_classes = decoders.Decoder.__subclasses__()
         init_state_batch = Variable(
-            torch.zeros(self.batch_size, self.num_features))
+            torch.zeros(self.batch_size, self.encoder_output_size))
         teacher_batch = Variable(
             torch.zeros(self.batch_size, self.caption_len).long())
         for decoder_class in decoder_classes:
