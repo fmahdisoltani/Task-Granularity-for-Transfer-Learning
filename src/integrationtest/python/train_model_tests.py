22cf32fac7fc53e53f38f11b9eafe3eea05761af
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2017-11-20 20:13:52 -0500

    Change parameter of checkpoint folder

--- src/integrationtest/python/train_model_tests.py
+++ src/integrationtest/python/train_model_tests.py
@@ -35,7 +35,7 @@ def simulate_training(config_path):
     # Check checkpoint folder
     check_saved_files(checkpoint_folder, ["config.yaml", "model.best",
                                                "model.latest",
-                                               "tokenizer_dicts_backup"])
+                                               "tokenizer_dicts"])
     return checkpoint_folder
 
 
