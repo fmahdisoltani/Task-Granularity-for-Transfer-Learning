54811e5e4978a7c2d48303afd774a540fbd4131f
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2017-11-22 20:26:22 -0500

    Update dimension tests

--- src/unittest/python/dimension_tests.py
+++ src/unittest/python/dimension_tests.py
@@ -37,7 +37,8 @@ class TestDimensions(unittest.TestCase):
                 (encoders.CNN3dLSTMEncoder, decoders.LSTMDecoder),
                 {"encoder_kwargs": {"encoder_output_size":  self.num_features},
                  "decoder_kwargs": {"embedding_size": 17,
-                                    "hidden_size": self.num_features,
+                                    "encoder_output_size":self.num_features,
+                                    "hidden_size": 11,
                                     "vocab_size": self.vocab_size,
                                     "num_lstm_layers": 1,
                                     "num_step": self.caption_len}}),
@@ -100,20 +101,20 @@ class TestDimensions(unittest.TestCase):
                     self.assertEqual(decoded.size()[2], self.vocab_size)
                     self.assertEqual(len(decoded.size()), 3)
 
-    # def test_captioners(self):
-    #     captioner_classes = captioners.Captioner.__subclasses__()
-    #     video_batch = (Variable(torch.zeros(self.batch_size, 3, 10, 96, 96)),
-    #                    Variable(torch.zeros(self.batch_size,
-    #                                         self.caption_len).long()))
-    #     for use_teacher_forcing in [True, False]:
-    #         for captioner_class in captioner_classes:
-    #             with self.subTest(captioner_class=captioner_class):
-    #                 self.assertIn(captioner_class.__name__, self.arguments)
-    #                 args, kwargs = self.arguments[captioner_class.__name__]
-    #
-    #                 captioner = captioner_class(*args, **kwargs)
-    #                 captioned = captioner(video_batch, use_teacher_forcing)
-    #
-    #                 self.assertEqual(captioned.size()[0], self.batch_size)
-    #                 self.assertEqual(captioned.size()[2], self.vocab_size)
-    #                 self.assertEqual(len(captioned.size()), 3)
+    def test_captioners(self):
+        captioner_classes = captioners.Captioner.__subclasses__()
+        video_batch = (Variable(torch.zeros(self.batch_size, 3, 10, 96, 96)),
+                       Variable(torch.zeros(self.batch_size,
+                                            self.caption_len).long()))
+        for use_teacher_forcing in [True, False]:
+            for captioner_class in captioner_classes:
+                with self.subTest(captioner_class=captioner_class):
+                    self.assertIn(captioner_class.__name__, self.arguments)
+                    args, kwargs = self.arguments[captioner_class.__name__]
+
+                    captioner = captioner_class(*args, **kwargs)
+                    captioned = captioner(video_batch, use_teacher_forcing)
+
+                    self.assertEqual(captioned.size()[0], self.batch_size)
+                    self.assertEqual(captioned.size()[2], self.vocab_size)
+                    self.assertEqual(len(captioned.size()), 3)
