7bd85e09f28ec571be32cd66ef293b9349c9818f
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2017-12-25 20:47:48 +0330

    Clean up encoders

--- src/main/python/ptcap/model/encoders.py
+++ src/main/python/ptcap/model/encoders.py
@@ -1,7 +1,4 @@
-import torch
 import torch.nn as nn
-
-from torch.autograd import Variable
 
 from ptcap.model.layers import CNN3dLayer
 from ptcap.tensorboardY import forward_hook_closure
@@ -11,92 +8,6 @@ class Encoder(nn.Module):
     def forward(self, video_batch):
         """BxCxTxWxH -> BxM"""
         raise NotImplementedError
-
-
-class FullyConnectedEncoder(Encoder):
-    def __init__(self, video_dims, num_features):
-        super(FullyConnectedEncoder, self).__init__()
-        C, T, W, H = video_dims
-        self.linear = nn.Linear(C * T * W * H, num_features)
-
-    def forward(self, video_batch):
-        batch_size = video_batch.size()[0]
-        return self.linear(video_batch.view(batch_size, -1))
-
-
-class CNN3dEncoder(Encoder):
-    def __init__(self, num_features=128, gpus=None):
-        super(CNN3dEncoder, self).__init__()
-
-        self.conv1 = CNN3dLayer(3, 16, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=1)
-        self.conv2 = CNN3dLayer(16, 32, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=1)
-        self.conv3 = CNN3dLayer(32, 64, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=1)
-
-        self.pool1 = nn.MaxPool3d((1, 2, 2))
-
-        self.pool2 = nn.MaxPool3d((1, 2, 2))
-
-        self.pool3 = nn.MaxPool3d((1, 2, 2))
-
-        self.conv4 = CNN3dLayer(64, 128, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=(1, 0, 0))
-        self.conv5 = CNN3dLayer(128, 128, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=(1, 0, 0))
-        self.conv6 = CNN3dLayer(128, num_features, (3, 3, 3), nn.ReLU(),
-                                stride=1, padding=(1, 0, 0))
-
-        self.pool4 = nn.MaxPool3d((1, 6, 6))
-
-        self.activations = self.register_forward_hooks()
-
-    def forward(self, videos):
-        # Video encoding
-        h = self.conv1(videos)
-        h = self.pool1(h)
-
-        h = self.conv2(h)
-        h = self.pool2(h)
-
-        h = self.conv3(h)
-        h = self.pool3(h)
-
-        h = self.conv4(h)
-        h = self.conv5(h)
-        h = self.conv6(h)
-
-        h = self.pool4(h)  # batch_size * num_features * num_step * w * h
-
-        h = h.mean(2)
-        h = h.view(h.size()[0:2])
-
-        return h
-
-    def register_forward_hooks(self):
-        master_dict = {}
-        self.conv1.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv1"))
-        self.conv2.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv2"))
-        self.conv3.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv3"))
-        self.conv4.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv4"))
-        self.conv5.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv5"))
-        self.conv6.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_conv6"))
-        self.pool1.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_pool1"))
-        self.pool2.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_pool2"))
-        self.pool3.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_pool3"))
-        self.pool4.register_forward_hook(
-            forward_hook_closure(master_dict, "encoder_pool4"))
-        return master_dict
 
 
 class C3dLSTMEncoder(Encoder):
@@ -112,14 +23,9 @@ class C3dLSTMEncoder(Encoder):
         self.encoder_output_size = encoder_output_size
         self.use_cuda = True if gpus else False
         self.gpus = gpus
-        
-        # self.logsoftmax = nn.LogSoftmax()
-        # self.classif_layer = nn.Linear(1024, self.num_classes)
+
         self.relu = nn.ReLU()
         self.fc = nn.Linear(self.encoder_output_size, 2* encoder_output_size)
-
-        #self.num_classes = 178
-        #self.classif_layer = nn.Linear(1024, self.num_classes)
 
         self.dropout = nn.Dropout(p=0.5)
 
@@ -148,22 +54,11 @@ class C3dLSTMEncoder(Encoder):
         lstm_hidden_size = int(
             self.encoder_output_size / 2) if bidirectional else self.encoder_output_size
 
-
         self.lstm = nn.LSTM(input_size=8*out_ch, hidden_size=lstm_hidden_size,
                             num_layers=self.num_layers, batch_first=True, 
                             bidirectional=True)
         
-        
-        
         self.activations = self.register_forward_hooks()
-
-    def init_hidden(self, batch_size):
-        h0 = Variable(torch.zeros(1, batch_size, self.encoder_output_size))
-        c0 = Variable(torch.zeros(1, batch_size, self.encoder_output_size))
-        if self.use_cuda:
-            h0 = h0.cuda(self.gpus[0])
-            c0 = c0.cuda(self.gpus[0])
-        return (h0, c0)
 
     def extract_features(self, videos):
         # videos: [batch_size*num_ch*len*w*h] (8*3*48*96*96)
@@ -190,15 +85,7 @@ class C3dLSTMEncoder(Encoder):
         self.lstm.flatten_parameters()
         lstm_outputs, _ = self.lstm(h) #lstm_input:8*48*256 lstm_output:8*48*1024
 
-        # h_mean = torch.mean(lstm_outputs, dim=1)
-
-        return self.dropout(self.relu(self.fc(lstm_outputs)))  # 8* 48 * 1024
-
-    # def predict_from_features(self, features):
-    #     probs = self.logsoftmax(features)
-    #     if probs.ndimension() == 3:
-    #         probs = probs.mean(dim=1)
-    #     return probs
+        return self.dropout(self.relu(self.fc(lstm_outputs)))  # [batch_size*num_step*num_features] (8*48*1024)
 
     def register_forward_hooks(self):
         master_dict = {}
