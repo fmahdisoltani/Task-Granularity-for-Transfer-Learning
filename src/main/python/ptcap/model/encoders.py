ef047b3e28292a7def654bc976a9ee92e53a47ed
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-01-14 19:18:34 -0500

    Refactor CNN3d encoder

--- src/main/python/ptcap/model/encoders.py
+++ src/main/python/ptcap/model/encoders.py
@@ -226,9 +226,10 @@ class C3dLSTMEncoder(Encoder):
             forward_hook_closure(master_dict, "encoder_lstm", 0, True))
         return master_dict
 
-class CNN3dEncoder(Encoder):
+
+class C3dEncoder(Encoder):
     def __init__(self, num_features=128, gpus=None):
-        super(CNN3dEncoder, self).__init__()
+        super(C3dEncoder, self).__init__()
 
         self.conv1 = CNN3dLayer(3, 16, (3, 3, 3), nn.ReLU(),
                                 stride=1, padding=1)
