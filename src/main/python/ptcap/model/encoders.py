e40a8f1d91dd9bbcc3e997054071c00f0e8e8b82
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-01-15 19:04:06 -0500

    Remove useless encoder

--- src/main/python/ptcap/model/encoders.py
+++ src/main/python/ptcap/model/encoders.py
@@ -13,17 +13,6 @@ class Encoder(nn.Module):
         raise NotImplementedError
 
 
-class FullyConnectedEncoder(Encoder):
-    def __init__(self, video_dims, num_features):
-        super(FullyConnectedEncoder, self).__init__()
-        C, T, W, H = video_dims
-        self.linear = nn.Linear(C * T * W * H, num_features)
-
-    def forward(self, video_batch):
-        batch_size = video_batch.size()[0]
-        return self.linear(video_batch.view(batch_size, -1))
-
-
 class C3dLSTMEncoder(Encoder):
     def __init__(self, encoder_output_size=52, gpus=None, out_ch=32,
                  bidirectional=True):
