1a645afb7649b398bce6177982887b9a284cae8a
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-02-16 18:31:48 -0500

    Read output channel size of streams from config file

--- src/main/python/ptcap/model/two_stream_encoders.py
+++ src/main/python/ptcap/model/two_stream_encoders.py
@@ -6,22 +6,22 @@ from ptcap.model.feature_extractors import C2dExtractor, C3dExtractor
 
 
 class TwoStreamEncoder(Encoder):
-    def __init__(self, encoder_output_size=52, c3d_output_size=0,
-                 c2d_output_size=0, rnn_output_size=51, bidirectional=True):
+    def __init__(self, encoder_output_size=52, c3d_out_ch=0,
+                 c2d_out_ch=0, rnn_output_size=51, bidirectional=True):
         super().__init__()
+        c3d_out_ch = 36
+        c2d_out_ch = 0
         self.encoder_output_size = encoder_output_size
 
-        self.use_c3d = c3d_output_size > 0
-        self.c3d_output_size = c3d_output_size
-        self.c3d_extractor = C3dExtractor()
+        self.use_c3d = c3d_out_ch > 0
+        self.c3d_extractor = C3dExtractor(out_ch=c3d_out_ch)
 
-        self.use_c2d = c2d_output_size > 0
-        self.c2d_output_size = c2d_output_size
-        self.c2d_extractor = C2dExtractor()
+        self.use_c2d = c2d_out_ch > 0
+        self.c2d_extractor = C2dExtractor(c2d_out_ch)
 
         lstm_hidden_size = int(rnn_output_size/2 if
                                bidirectional else rnn_output_size)
-        self.lstm = nn.LSTM(input_size=c3d_output_size+c2d_output_size,
+        self.lstm = nn.LSTM(input_size=8*c3d_out_ch + 8*c2d_out_ch,
                             hidden_size=lstm_hidden_size, num_layers=1,
                             batch_first=True, bidirectional=True)
 
