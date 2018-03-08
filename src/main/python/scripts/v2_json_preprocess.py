dd391e7c34bb2b789271213aab94560811e58501
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-03-08 00:06:27 -0500

    Add test file to v2 json preprocessor

--- src/main/python/scripts/v2_json_preprocess.py
+++ src/main/python/scripts/v2_json_preprocess.py
@@ -35,15 +35,17 @@ def open_json(path):
 
 if __name__ == "__main__":
     input_jsons = ["/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/something-something-v2-train.json",
-                   "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/something-something-v2-validation.json"]
+                   "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/something-something-v2-validation.json",
+                   "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/something-something-v2-test.json"]
     output_jsons = [
-        "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/v2-train-simple.json",
-        "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/v2-validation-simple.json"]
+        "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/v2-train-single.json",
+        "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/v2-validation-single.json",
+        "/home/farzaneh/PycharmProjects/pytorch-captioning/src/main/python/scripts/v2-test-single.json"]
 
 
     for in_json, out_json in zip(input_jsons, output_jsons):
         print("Creating subset from {} to {}".format(in_json, out_json))
-        new_json = json_process(in_json, True)
+        new_json = json_process(in_json, simple=True)
 
         with open(out_json, "w") as f:
             json.dump(new_json, f)
