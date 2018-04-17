e27909806400f0d895189cb7653dbb9fa4df689e
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-17 16:46:31 -0400

    Implement RL agent

---
+++ src/main/python/ptcap/real_time_captioning/agent.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch
+
+
+class Agent:
+    def __init__(self, input_size, hidden_size, output_size,
+                 encoder, decoder, encoder_args=None, encoder_kwargs=None,
+                 decoder_args=None, decoder_kwargs=None,):
+
+
+        self.policy = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+            nn.Softmax()
+        )
+
+    def get_action_probs(self, x):
+        action_probs = self.policy(x)
+        return action_probs
+
+    def select_action(self, state):
+        action_probs = self.get_action_probs(state)
+        m = torch.distributions.Categorical(action_probs)
+        action = m.sample()
+        log_prob = torch.sum(m.log_prob(action))
+        return action, log_prob
+
+    def update_policy(self, logprobs_seq, reward_seq, gamma=1.0):
+        policy_loss = []
+        returns = self.compute_returns(reward_seq, gamma)
+        returns = torch.Tensor(returns)
+        # subtract mean and std for faster training
+        returns = (returns - returns.mean()) / (returns.std() +
+                                                np.finfo(np.float32).eps)
+        for log_prob, reward in zip(logprobs_seq, returns):
+            policy_loss.append(-log_prob * reward)
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward(retain_graph=True)
+        # note: retain_graph=True allows for multiple calls to .backward()
+        # in a single step
+
+    def compute_returns(self,rewards, gamma=1.0):
+        """
+        Compute returns for each time step, given the rewards
+          @param rewards: list of floats, where rewards[t] is the reward
+                          obtained at time step t
+          @param gamma: the discount factor
+          @returns list of floats representing the episode's returns
+              G_t = r_t + \gamma r_{t+1} + \gamma^2 r_{t+2} + ...
+        """
+        G_last = 0
+        G = []
+        for r in reversed(rewards):
+            G_t = G_last * gamma + r
+            G.append(G_t)
+            G_last = G_t
+        G.reverse()
+        return G
