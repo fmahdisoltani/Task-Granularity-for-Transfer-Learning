467506aa3abc5297ef3aa38d9223d2f2a30db1ba
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-18 17:51:02 -0400

    Fix update_policy arguments

--- src/main/python/ptcap/real_time_captioning/agent.py
+++ src/main/python/ptcap/real_time_captioning/agent.py
@@ -1,9 +1,12 @@
 import torch.nn as nn
 import torch
+import numpy as np
+
+from torch.autograd import Variable
 
 
 class Agent:
-    def __init__(self, input_size=20, hidden_size=33, output_size=2):
+    def __init__(self, input_size=1, hidden_size=33, output_size=2):
 
         self.policy = nn.Sequential(
             nn.Linear(input_size, hidden_size),
@@ -13,9 +16,13 @@ class Agent:
         )
 
     def get_action_probs(self, x):
+        #TODO: Fix x
+        x=Variable(torch.zeros(1))
         action_probs = self.policy(x)
         return action_probs
-
+    def prepare_policy_input(self, state):
+        last_token = state.output_buffer[-1]
+        return
     def select_action(self, state):
         action_probs = self.get_action_probs(state)
         m = torch.distributions.Categorical(action_probs)
@@ -23,19 +30,7 @@ class Agent:
         log_prob = torch.sum(m.log_prob(action))
         return action, log_prob
 
-    def update_policy(self, logprobs_seq, reward_seq, gamma=1.0):
-        policy_loss = []
-        returns = self.compute_returns(reward_seq, gamma)
-        returns = torch.Tensor(returns)
-        # subtract mean and std for faster training
-        returns = (returns - returns.mean()) / (returns.std() +
-                                                np.finfo(np.float32).eps)
-        for log_prob, reward in zip(logprobs_seq, returns):
-            policy_loss.append(-log_prob * reward)
-        policy_loss = torch.cat(policy_loss).sum()
-        policy_loss.backward(retain_graph=True)
-        # note: retain_graph=True allows for multiple calls to .backward()
-        # in a single step
+
 
     def compute_returns(self,rewards, gamma=1.0):
         """
@@ -54,3 +49,18 @@ class Agent:
             G_last = G_t
         G.reverse()
         return G
+
+    def update_policy(self, reward_seq, logprobs_seq, gamma=1.0):
+
+        policy_loss = []
+        returns = self.compute_returns(reward_seq, gamma)
+        returns = torch.Tensor(returns)
+        # subtract mean and std for faster training
+        returns = (returns - returns.mean()) / (returns.std() +
+                                                np.finfo(np.float32).eps)
+        for log_prob, reward in zip(logprobs_seq, returns):
+            policy_loss.append(-log_prob * reward)
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward(retain_graph=True)
+        # note: retain_graph=True allows for multiple calls to .backward()
+        # in a single step
