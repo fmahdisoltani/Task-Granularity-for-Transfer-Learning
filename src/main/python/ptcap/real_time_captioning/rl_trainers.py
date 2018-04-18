4615b618c8fc9234ebba6c8bee34df2f6a51c0bb
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-17 23:39:32 -0400

    Clean up RLTrainer

--- src/main/python/ptcap/real_time_captioning/rl_trainers.py
+++ src/main/python/ptcap/real_time_captioning/rl_trainers.py
@@ -14,24 +14,22 @@ from ptcap.real_time_captioning.agent import Agent
 
 
 class RLTrainer(object):
-    def __init__(self, model, caption_loss_function, w_caption_loss, scheduler,
-                 tokenizer, logger,
-                 writer, checkpointer, load_encoder_only, folder=None,
-                 filename=None,
-                 gpus=None, clip_grad=None, classif_loss_function=None,
-                 w_classif_loss=0):
-
+    def __init__(self, encoder,decoder, caption_loss_function, tokenizer, gpus=None):
 
         self.gpus = gpus
-        self.model = model if self.gpus is None else(
-            DataParallelWrapper(model, device_ids=self.gpus).cuda(gpus[0])
+        self.use_cuda = True if gpus else False
+        self.encoder = encoder if self.gpus is None else(
+            DataParallelWrapper(encoder, device_ids=self.gpus).cuda(gpus[0])
+        )
+        self.decoder = decoder if self.gpus is None else(
+            DataParallelWrapper(decoder, device_ids=self.gpus).cuda(gpus[0])
         )
         self.loss_function = caption_loss_function if self.gpus is None else(
             caption_loss_function.cuda(gpus[0])
         )
 
         self.tokenizer = tokenizer
-        self.env = Environment()
+        self.env = Environment(self.encoder, self.decoder)
         self.agent = Agent()
 
     def get_input_captions(self, captions, use_teacher_forcing):
@@ -41,61 +39,49 @@ class RLTrainer(object):
             input_captions = torch.cat([input_captions, captions[:, :-1]], 1)
         return input_captions
 
-    def train(self, train_dataloader, valid_dataloader, criteria,
-              max_num_epochs=None, frequency_valid=1, teacher_force_train=True,
-              teacher_force_valid=False, verbose_train=False,
-              verbose_valid=False):
+    def train(self, train_dataloader, teacher_force_train=True,
+               verbose_train=False):
         for i_episode in range(1000):
             self.run_episode(train_dataloader,i_episode, is_training=True,
                              use_teacher_forcing=teacher_force_train,
                              verbose=verbose_train)
 
-
-
     def run_episode(self, dataloader, epoch, is_training,
                   use_teacher_forcing=False, verbose=True):
+        print("*****inside episode*****{}".format("%"*3))
 
 
-        if is_training:
-            self.model.train()
-        else:
-            self.model.eval()
+        #if is_training: self.model.train()
+        #else: self.model.eval()
 
-        for i_episode, (videos, _, captions, _) in enumerate(
-                dataloader):
+        for i_episode, (videos, _, captions, _) in enumerate(dataloader):
 
             input_captions = self.get_input_captions(captions,
                                                      use_teacher_forcing)
 
-            videos, captions, input_captions = (
-            Variable(videos),
-            Variable(captions),
+            videos, captions, input_captions = (Variable(videos),Variable(captions),
             Variable(input_captions))
             if self.use_cuda:
                 videos = videos.cuda(self.gpus[0])
                 captions = captions.cuda(self.gpus[0])
                 input_captions = input_captions.cuda(self.gpus[0])
 
-
-            env.reset(videos, input_Captions)
+            # Reset the RL environment
+            self.env.reset(videos, input_captions)
             finished = False
             reward_seq = []
             action_seq = []
             logprob_seq = []
             while not finished:
-                state = env.get_state()
-                action, logprob = agent.select_action(state)
+                state = self.env.get_state()
+                action, logprob = self.agent.select_action(state)
                 action_seq.append(action)
                 logprob_seq.append(logprob)
-                reward = env.update_state(action)
+                reward = self.env.update_state(action)
                 reward_seq.append(reward)
-                finished = env.check_finished()
-            agent.update_policy(reward_seq, logprob_seq)
+                finished = self.env.check_finished()
+            self.agent.update_policy(reward_seq, logprob_seq)
 
-            if is_training:
-
-                self.model.zero_grad()
-                loss.backward()
 
 
 
