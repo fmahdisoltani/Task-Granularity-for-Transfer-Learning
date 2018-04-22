4f0c2b28d20cba921fd16c51b633559ed5295a4a
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-21 22:14:41 -0400

    Remove unused class

--- src/main/python/ptcap/model/layers.py
+++ src/main/python/ptcap/model/layers.py
@@ -76,34 +76,3 @@ class CausalC3dLayer(nn.Module):
             h = self.batchnorm(h)
 
         return self.activation(h)
-
-
-class CausalC3dLayer2(nn.Module):
-    # stupid comment
-    def __init__(self, in_channels, out_channels, kernel_size, activation,
-                 stride=1, padding=0, dilation=1, groups=1, bias=True,
-                  batchnorm=True):
-
-        super().__init__()
-
-        t = kernel_size[0]
-        self.pad = nn.ConstantPad3d((padding, padding, padding, padding,
-                                    2*dilation, 0), value=0)
-
-        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size,
-                              stride, padding, dilation, groups, bias)
-        self.activation = activation
-
-        if batchnorm:
-            self.batchnorm = nn.BatchNorm3d(out_channels)
-        else:
-            self.batchnorm = None
-
-    def forward(self, x):
-        h = self.conv(x)
-        if self.batchnorm:
-            h = self.batchnorm(h)
-
-        return self.activation(h)
-
-
