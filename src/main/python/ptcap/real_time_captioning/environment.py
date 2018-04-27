37faa3e9fc162258d2d55f0b523b209eb1e50935
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-27 12:57:49 -0400

    Add classif layer

--- src/main/python/ptcap/real_time_captioning/environment.py
+++ src/main/python/ptcap/real_time_captioning/environment.py
@@ -1,22 +1,28 @@
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.autograd import Variable
 from collections import namedtuple
 
 
 class Environment:
     # statuses
-    STATUS_VALID_WRITE = 'write'
-    STATUS_VALID_ACTION = 'valid'
-    STATUS_INVALID_ACTION = 'invalid'
+    STATUS_CORRECT_WRITE = 'correct_write'
+    STATUS_INCORRECT_WRITE = 'incorrect_write'
+    STATUS_READ = 'read'
     STATUS_DONE = 'done'
 
     def __init__(self, encoder):
 
         self.encoder = encoder
+        self.classif_layer = \
+            nn.Linear(self.encoder.encoder_output_size, 174)
+        self.classif_layer = self.classif_layer.cuda()
+        self.logsoftmax = nn.LogSoftmax(dim=-1)
+
         #self.reset()
 
-    def reset(self, video=None, caption=None):
+    def reset(self, video=None, caption=None, classif=None):
         self.read_count = 0
         self.write_count = 0
 
@@ -32,32 +38,43 @@ class Environment:
             "input_buffer": self.input_buffer
         }
 
-    def update_state(self, action, action_seq=[]):
+    def update_state(self, action, action_seq=[], classif_targets=None):
         status = ""
 
         if action.data.numpy()[0] == 0:  # READ
-            status = Environment.STATUS_VALID_ACTION
+            status = Environment.STATUS_READ
             self.input_buffer.append(self.vid_encoding[:, self.read_count, :])
             self.read_count += 1
 
         if action.data.numpy()[0] == 1:  # WRITE
-            if len(action_seq) > 2 and action_seq[-2]+action_seq[-3] == 0:
-                status = Environment.STATUS_VALID_WRITE
-
+            classif_probs = self.classify()
+            _, prediction = torch.max(classif_probs, dim=1)
+            #print(prediction.data.cpu().numpy()[0])
+            #print(classif_targets.data.cpu().numpy()[0])
+            if prediction.data.cpu().numpy()[0] == classif_targets.data.cpu().numpy()[0]:
+                status = Environment.STATUS_CORRECT_WRITE
             else:
-
-                status = Environment.STATUS_INVALID_ACTION
+                status = Environment.STATUS_INCORRECT_WRITE
             self.write_count += 1
 
         reward = self.give_reward(status)
         return reward
 
     def check_finished(self):
-        return self.write_count+self.read_count == 10
+        return self.write_count == 1
 
     def give_reward(self, status):
         return {
-            Environment.STATUS_VALID_WRITE: 10,
-            Environment.STATUS_VALID_ACTION: 1,
-            Environment.STATUS_INVALID_ACTION: -1,
+            Environment.STATUS_CORRECT_WRITE: 100,
+            Environment.STATUS_INCORRECT_WRITE: -1000,
+            Environment.STATUS_READ: -1,
         }[status]
+
+    def classify(self):
+        features = self.input_buffer[-1]
+        pre_activation = self.classif_layer(features)
+        probs = self.logsoftmax(pre_activation)
+        if probs.ndimension() == 3:
+            probs = probs.mean(dim=1)  # probs: [8*48*178]
+
+        return probs
