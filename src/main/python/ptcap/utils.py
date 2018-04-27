1377d0b93a99f3af132b0bcef83d4667f55e3286
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-04-27 12:58:28 -0400

    Minor

--- src/main/python/ptcap/utils.py
+++ src/main/python/ptcap/utils.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class DataParallelWrapper(nn.DataParallel):
     def __init__(self, model, device_ids):
         super().__init__(model, device_ids=device_ids)
-        #self.gpus = device_ids
+        self.encoder_output_size = self.module.encoder_output_size
 
     @property
     def activations(self):
