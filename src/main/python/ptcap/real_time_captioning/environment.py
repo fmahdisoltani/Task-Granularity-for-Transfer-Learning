e92d9a85ec3a05dffb09adac3137db1da1f3824a
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-05-08 17:00:05 -0400

    Incorporate loaded classif layer

--- src/main/python/ptcap/real_time_captioning/environment.py
+++ src/main/python/ptcap/real_time_captioning/environment.py
@@ -13,16 +13,14 @@ class Environment(nn.Module):
     STATUS_READ = 'read'
     STATUS_DONE = 'done'
 
-    def __init__(self, encoder):
+    def __init__(self, encoder, classif_layer):
         super().__init__()
 
         self.encoder = encoder
 
         self.logsoftmax = nn.LogSoftmax(dim=-1)
-        self.classif_layer = \
-                      nn.Linear(self.encoder.encoder_output_size, 174)
+        self.classif_layer = classif_layer
 
-        self.classif_layer = self.classif_layer.cuda()
         self.logsoftmax = nn.LogSoftmax(dim=-1)
 
         #self.reset()
@@ -33,8 +31,7 @@ class Environment(nn.Module):
 
         self.vid_encoding = self.encoder.extract_features(video)
         # input buffer contains the seen video frames
-        self.input_buffer = [self.vid_encoding[:, 0,:]]
-
+        self.input_buffer = [self.vid_encoding[:, 0, :]]
 
     def get_state(self):
         return {
@@ -46,18 +43,18 @@ class Environment(nn.Module):
     def update_state(self, action, action_seq=[], classif_targets=None):
         status = ""
         classif_probs = self.classify()
+        value_prob = None
+
         if action.data.numpy()[0] == 0:  # READ
-            if self.read_count == 48:
+            if self.read_count == 47:
                 status = Environment.STATUS_INVALID_READ
             else:
                 status = Environment.STATUS_READ
                 self.input_buffer.append(self.vid_encoding[:, self.read_count, :])
                 self.read_count += 1
 
-
         if action.data.numpy()[0] == 1:  # WRITE
-
-            _, prediction = torch.max(classif_probs, dim=1)
+            value_prob, prediction = torch.max(classif_probs, dim=1)
 
             if prediction.data.cpu().numpy()[0] == classif_targets.data.cpu().numpy()[0]:
                 status = Environment.STATUS_CORRECT_WRITE
@@ -65,24 +62,26 @@ class Environment(nn.Module):
                 status = Environment.STATUS_INCORRECT_WRITE
             self.write_count += 1
 
-        reward = self.give_reward(status)
+        reward = self.give_reward(status, value_prob)
     #    reward = torch.sum(classif_probs)
-        return reward
+        return reward, classif_probs
 
     def check_finished(self):
-        return self.write_count == 1
+        return self.write_count == 1 or self.read_count == 48
 
-    def give_reward(self, status):
-        return {
+    def give_reward(self, status, value_prob=None):
+        r =  {
             Environment.STATUS_CORRECT_WRITE: 100,
-            Environment.STATUS_INCORRECT_WRITE: -1000,
-            Environment.STATUS_READ: -1,
+            Environment.STATUS_INCORRECT_WRITE: -100,
+            Environment.STATUS_READ: 1,
             Environment.STATUS_INVALID_READ: -10,
         }[status]
+        #r = r if value_prob is None else (-1/(1+value_prob)) * r
+        return r
 
     def classify(self):
         features = self.input_buffer[-1]
-        pre_activation = self.classif_layer(features)
+        pre_activation = self.classif_layer(self.vid_encoding[:, self.read_count, :])
         probs = self.logsoftmax(pre_activation)
         if probs.ndimension() == 3:
             probs = probs.mean(dim=1)  # probs: [8*48*178]
