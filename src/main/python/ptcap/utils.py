92e50d3e86b6c3ff191bdd2d445f609858e6120d
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-06-02 00:37:58 -0400

    Update utils

--- src/main/python/ptcap/utils.py
+++ src/main/python/ptcap/utils.py
@@ -1,12 +1,14 @@
-
 import torch.nn as nn
 
 
 class DataParallelWrapper(nn.DataParallel):
     def __init__(self, model, device_ids):
         super().__init__(model, device_ids=device_ids)
-        self.gpus = model.gpus
+        self.encoder_output_size = self.module.encoder_output_size
 
     @property
     def activations(self):
         return self.module.activations
+
+    def extract_features(self, x):
+        return self.module.extract_features(x)
