281caf69ca4ce4eb25afca481c520fef84f450f3
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-06-04 00:21:54 -0400

    Update

--- src/main/python/ptcap/utils.py
+++ src/main/python/ptcap/utils.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class DataParallelWrapper(nn.DataParallel):
     def __init__(self, model, device_ids):
         super().__init__(model, device_ids=device_ids)
-        self.encoder_output_size = self.module.encoder_output_size
+#        self.encoder_output_size = self.module.encoder_output_size
 
     @property
     def activations(self):
