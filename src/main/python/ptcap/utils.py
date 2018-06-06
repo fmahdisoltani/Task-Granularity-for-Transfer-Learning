0648c50bf010155cf3dc7708ed6f13f88966b2ce
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-06-06 13:06:38 -0400

    Add custom subset sampler to utils

--- src/main/python/ptcap/utils.py
+++ src/main/python/ptcap/utils.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+from torch.utils.data.sampler import Sampler
 
 
 class DataParallelWrapper(nn.DataParallel):
@@ -12,3 +14,28 @@ class DataParallelWrapper(nn.DataParallel):
 
     def extract_features(self, x):
         return self.module.extract_features(x)
+
+class CustomSubsetSampler(Sampler):
+
+    def __init__(self, subset_size, total_size):
+        self.subset_size = subset_size
+        self.total_size = total_size
+        self.reset_ids()
+
+    def reset_ids(self):
+        self.ids = torch.randperm(self.total_size)
+
+    def __iter__(self):
+        end_index = min(self.subset_size, len(self.ids))
+        subset = self.ids[0:end_index]
+        if len(self.ids) > self.subset_size:
+            self.ids = self.ids[end_index:]
+        else:
+            self.reset_ids()
+        print(subset)
+        return iter(subset)
+
+    def __len__(self):
+        return self.subset_size
+
+
