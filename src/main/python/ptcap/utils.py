62fcf87e8f47bb38a1979874e3a2b253f731cbc1
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-06-20 23:39:14 -0400

    Fix bug in utils

--- src/main/python/ptcap/utils.py
+++ src/main/python/ptcap/utils.py
@@ -28,16 +28,17 @@ class CustomSubsetSampler(Sampler):
 
     def __iter__(self):
         if len(self.inds) < self.subset_size:
-            new_indices = self.reset_inds()
-            self.inds = list(self.inds) + list(new_indices)
+            old_inds = self.inds
+            self.reset_inds()
+            self.inds = list(old_inds) + list(self.inds)
         subset = self.inds[0:self.subset_size]
         self.inds = self.inds[self.subset_size:]
 
-        #if len(self.inds) == 0:
+        # if len(self.inds) == 0:
         #    self.reset_inds()
-        #end_index = min(self.subset_size, len(self.inds))
-        #self.inds = self.inds[end_index:]
-        #subset = self.inds[0:end_index]
+        # end_index = min(self.subset_size, len(self.inds))
+        # self.inds = self.inds[end_index:]
+        # subset = self.inds[0:end_index]
 
         return iter(subset)
 
