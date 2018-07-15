982e0c074719a3bf1df6263e86a58135aa158734
fmahdisoltani <farzaneh.mahdisoltani@twentybn.com>
2018-07-15 15:26:14 -0400

    incorporate ce

--- src/main/python/ptcap/real_time_captioning/classif_env.py
+++ src/main/python/ptcap/real_time_captioning/classif_env.py
@@ -61,7 +61,7 @@ class ClassifEnv(nn.Module):
         status = ""
         classif_probs = self.classify()
         value_prob = None
-
+        ce_value = 1
         if action == 0:  # READ
             if self.read_count == 47:
                 status = ClassifEnv.STATUS_INVALID_READ
@@ -79,19 +79,21 @@ class ClassifEnv(nn.Module):
                 status = ClassifEnv.STATUS_INCORRECT_WRITE
 
             self.write_count += 1
+            ce_value = torch.max(classif_probs).data[0]
 
-        reward = self.give_reward(status, value_prob)
+        reward = self.give_reward(status, value_prob, classif_probs, classif_targets)
     #    reward = torch.sum(classif_probs)
-        return reward, classif_probs
+        return reward*ce_value, classif_probs
 
     def check_finished(self):
         return self.write_count == 1 or self.read_count == 48
 
-    def give_reward(self, status, value_prob=None):
+    def give_reward(self, status, value_prob=None,
+                    classif_probs=None,classif_targets=None):
         r = {
-            ClassifEnv.STATUS_CORRECT_WRITE: self.correct_w_reward,
+            ClassifEnv.STATUS_CORRECT_WRITE: self.correct_w_reward, #  classif_probs[0,classif_targets.data.cpu().numpy()[0]],
             ClassifEnv.STATUS_READ: self.correct_r_reward,
-            ClassifEnv.STATUS_INCORRECT_WRITE: self.incorrect_w_reward,
+            ClassifEnv.STATUS_INCORRECT_WRITE: self.incorrect_w_reward,#*
             ClassifEnv.STATUS_INVALID_READ: self.incorrect_r_reward,
         }[status]
         #r = r if value_prob is None else (-1/(1+value_prob)) * r
